b6af24a1f14d24bed78fce6800989b34d3882f4c
Eric Donnelly <200433532@student.georgianc.on.ca>
2023-06-24 22:40:44 -0400

    This is the start of where this needs to be changed to work properly, comments are in index.ejs, Controllers/index.ts and I moved the files around as they need to be

--- Server/Controllers/index.js
+++ Server/Controllers/index.js
@@ -1,12 +1,70 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DisplayContactPage = exports.DisplayProjectsPage = exports.DisplayServicesPage = exports.DisplayAboutPage = exports.DisplayHomePage = void 0;
+const team = [
+    {
+        name: "Rajanpreet Singh",
+        role: "Lead Software Engineer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+        imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+    },
+    {
+        name: "Eric Donnelly",
+        role: "Project Manager",
+        bio: "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+        imgUrl: "/assets/images/crewImages/EricPic.jpg",
+    },
+    {
+        name: "Rajanpreet Singh",
+        role: "Lead Software Engineer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+        imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+    },
+    {
+        name: "Eric Donnelly",
+        role: "Project Manager",
+        bio: "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+        imgUrl: "/assets/images/crewImages/EricPic.jpg",
+    },
+    {
+        name: "Rajanpreet Singh",
+        role: "Lead Software Engineer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+        imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+    },
+    {
+        name: "Eric Donnelly",
+        role: "Project Manager",
+        bio: "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+        imgUrl: "/assets/images/crewImages/EricPic.jpg",
+    },
+    {
+        name: "Rajanpreet Singh",
+        role: "Lead Software Engineer",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+        imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+    },
+    {
+        name: "Eric Donnelly",
+        role: "Project Manager",
+        bio: "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+        imgUrl: "/assets/images/crewImages/EricPic.jpg",
+    },
+];
 function DisplayHomePage(req, res, next) {
-    res.render("index", { title: "Home", page: "home" });
+    res.render("index", {
+        title: "Home",
+        page: "home",
+        team: team,
+    });
 }
 exports.DisplayHomePage = DisplayHomePage;
 function DisplayAboutPage(req, res, next) {
-    res.render("index", { title: "About", page: "about" });
+    res.render("index", {
+        title: "About",
+        page: "about",
+        team: team,
+    });
 }
 exports.DisplayAboutPage = DisplayAboutPage;
 function DisplayServicesPage(req, res, next) {
--- Server/Controllers/index.ts
+++ Server/Controllers/index.ts
@@ -1,13 +1,79 @@
 import express from "express";
 
 // controller logic to display the home page
+// This is where we will define the team array, objects are used to create the cards for About Us Page
+// TODO: these need to be updated with the appropriate objects 1 for everyone , that url path is correct for where the images have been moved just need the picture name fixed
+const team = [
+  {
+    name: "Rajanpreet Singh",
+    role: "Lead Software Engineer",
+    bio:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+    imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+  },
+  {
+    name: "Eric Donnelly",
+    role: "Project Manager",
+    bio:
+      "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+    imgUrl: "/assets/images/crewImages/EricPic.jpg",
+  },
+  {
+    name: "Rajanpreet Singh",
+    role: "Lead Software Engineer",
+    bio:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+    imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+  },
+  {
+    name: "Eric Donnelly",
+    role: "Project Manager",
+    bio:
+      "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+    imgUrl: "/assets/images/crewImages/EricPic.jpg",
+  },
+  {
+    name: "Rajanpreet Singh",
+    role: "Lead Software Engineer",
+    bio:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+    imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+  },
+  {
+    name: "Eric Donnelly",
+    role: "Project Manager",
+    bio:
+      "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+    imgUrl: "/assets/images/crewImages/EricPic.jpg",
+  },
+  {
+    name: "Rajanpreet Singh",
+    role: "Lead Software Engineer",
+    bio:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis sit amet tortor cursus aliquam.",
+    imgUrl: "/assets/images/crewImages/RajanpreetPic.jpg",
+  },
+  {
+    name: "Eric Donnelly",
+    role: "Project Manager",
+    bio:
+      "Eric has 4 years of development experience primarily in creating and maintaining web-based applications. He is constantly improving his technical and team management skills. For Giga Crew, his role includes developing the roadmap and release cycle for Dream Journal , along with keeping the project moving forward as needed.",
+    imgUrl: "/assets/images/crewImages/EricPic.jpg",
+  },
+
+  // Add additional Members as needed
+];
 
 export function DisplayHomePage(
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ): void {
-  res.render("index", { title: "Home", page: "home" });
+  res.render("index", {
+    title: "Home",
+    page: "home",
+    team: team,
+  });
 }
 
 export function DisplayAboutPage(
@@ -15,7 +81,11 @@ export function DisplayAboutPage(
   res: express.Response,
   next: express.NextFunction
 ): void {
-  res.render("index", { title: "About", page: "about" });
+  res.render("index", {
+    title: "About",
+    page: "about",
+    team: team,
+  });
 }
 
 export function DisplayServicesPage(
